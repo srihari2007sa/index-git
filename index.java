0fa52ea9c17621af88f742e22f75682eb47543dc
srihari2007sa <srihari2007sa@gmail.com>
2026-05-19 14:40:32 +0530

    first commit

---
+++ index.java
@@ -0,0 +1,32 @@
+class arithmeticalOperations { 
+    int a, b;
+    arithmeticalOperations(int x, int y) {
+        a = x;
+        b = y;
+    }
+    void add() {
+        System.out.println("Addition: " + (a + b));
+    }
+    void subtract() {   
+        System.out.println("Subtraction: " + (a - b));
+    }   
+    void multiply() {
+        System.out.println("Multiplication: " + (a * b));
+    }
+    void divide() {
+        if (b != 0) {
+            System.out.println("Division: " + (a / b));
+        } else {
+            System.out.println("Division by zero is not allowed.");
+        }
+    }
+}
+public class index {
+    public static void main(String[] args) {
+        arithmeticalOperations operations = new arithmeticalOperations(10, 5);
+        operations.add();
+        operations.subtract();
+        operations.multiply();
+        operations.divide();
+    }
+}
